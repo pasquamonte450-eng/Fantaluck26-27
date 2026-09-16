4973def99f56159df8c61d2b13a909944e3431b6
pasquamonte450-eng <pasquamonte450@gmail.com>
2026-09-16 14:27:09 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1997,7 +1997,7 @@ function PronosticiRank({
         </div>
 
         <div>
-          <strong> style={{ marginBottom: "8px" }}>
+          <strong> style={{ display: "block", marginBottom: "8px" }}>
             PRONOSTICI INDOVINATI
           </strong>
 
